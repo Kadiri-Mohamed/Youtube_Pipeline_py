22c376e744baa50cdfda1f4380c4c1a848d10fd1
Kadiri <kadiri.moohamed@gmail.com>
2026-09-16 15:30:27 +0100

    feat: Implement update_staging task to insert videos into PostgreSQL staging table

--- dags/youtube_warehouse_dag.py
+++ dags/youtube_warehouse_dag.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 
 from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 @dag(
@@ -32,8 +33,69 @@ def youtube_warehouse():
         print(f"Number of videos: {len(videos)}")
 
         return videos
+    @task
+    def update_staging(videos):
 
-    read_json()
+        hook = PostgresHook(
+            postgres_conn_id="postgres_db_yt_elt"
+        )
+
+        connection = hook.get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS staging;
+
+            CREATE TABLE IF NOT EXISTS staging.youtube_videos (
+                video_id VARCHAR(20) PRIMARY KEY,
+                title TEXT,
+                published_at TIMESTAMP,
+                duration VARCHAR(50),
+                views BIGINT,
+                likes BIGINT,
+                comments BIGINT
+            );
+        """)
+
+        for video in videos:
+            cursor.execute("""
+                INSERT INTO staging.youtube_videos (
+                    video_id,
+                    title,
+                    published_at,
+                    duration,
+                    views,
+                    likes,
+                    comments
+                )
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (video_id)
+                DO UPDATE SET
+                    title = EXCLUDED.title,
+                    published_at = EXCLUDED.published_at,
+                    duration = EXCLUDED.duration,
+                    views = EXCLUDED.views,
+                    likes = EXCLUDED.likes,
+                    comments = EXCLUDED.comments;
+            """, (
+                video["video_id"],
+                video["title"],
+                video["published_at"],
+                video["duration"],
+                video["views"],
+                video["likes"],
+                video["comments"]
+            ))
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        print(f"Staging updated with {len(videos)} videos")
+
+    videos = read_json()
+    update_staging(videos)
 
 
 youtube_warehouse()
